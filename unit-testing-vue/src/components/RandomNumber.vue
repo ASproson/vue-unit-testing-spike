e0e7c0805b159b97df7561661f977e2c7c05086d
Atlas Sproson <atlas.sproson@r2-factory.com>
2022-04-20 11:41:59 +0100

    Adds emission testing

--- unit-testing-vue/src/components/RandomNumber.vue
+++ unit-testing-vue/src/components/RandomNumber.vue
@@ -24,10 +24,8 @@ export default {
   },
   methods: {
     getRandomNumber() {
-      //   this.randomNumber = Math.floor(Math.random() * (this.min + 1)) + this.min
       this.randomNumber =
         Math.floor(Math.random() * (this.max - this.min + 1)) + this.min
-      //   Math.floor(Math.random() * (max - min + 1)) + min;
     },
   },
 }
